614ec2db1f477d81b586cd706f2ef032c95c45cd
Benjamin Awad <awad@sipgate.de>
2021-11-23 10:15:06 +0100

    :sparkles: initialize constants using .env
    Co-authored-by: Malte Sehmer <sehmer@sipgate.de>

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,25 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const msg = "Hello, Typescript!";
-console.log(msg);
+if (!process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS) {
+  console.error(
+    "ERROR: You need to set a server address to receive webhook events!\n",
+  );
+  process.exit();
+}
+
+if (!process.env.SIPGATE_WEBHOOK_SERVER_PORT) {
+  console.error(
+    "ERROR: You need to set a server port to receive webhook events!\n",
+  );
+  process.exit();
+}
+
+if (!process.env.ANNOUNCEMENT_FILE_URL) {
+  console.error("ERROR: You need to specify an announcement file!\n");
+  process.exit();
+}
+
+const SERVER_ADDRESS = process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS;
+const PORT = process.env.SIPGATE_WEBHOOK_SERVER_PORT;
+const { ANNOUNCEMENT_FILE_URL } = process.env;
